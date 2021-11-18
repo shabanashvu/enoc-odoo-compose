8ce20bef1e386a520958a23cca028e2dadfd2c88
shabanashvu <54355254+shabanashvu@users.noreply.github.com>
2021-11-18 21:25:40 +0400

    updated the DB host Details and Dependable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,13 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='172.104.55.52'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='shabz'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='Eshaanchoot1'}}}
 
 # install python packages
+pip3 install pyzk
 pip3 install pip --upgrade
 pip3 install -r /etc/odoo/requirements.txt
 
